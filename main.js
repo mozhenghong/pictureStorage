25f814543c8d8da67bce28fbcd932c38028f270c
momo <1445107381@qq.com>
2018-11-29 12:18:25 +0800

    update js

--- main.js
+++ main.js
@@ -6,3 +6,11 @@ function showpic(whichpic){
     // var description = document.getElementById('description');
     description.firstChild.nodeValue = text;
 }
+var gollary = document.getElementById('gollary');
+var link = gollary.getElementsByTagName('a');
+for(var i=0;i<link.length;i++){
+    link[i].onclick = function(){ 
+        showpic(this);
+        return false;  
+    }
+}
